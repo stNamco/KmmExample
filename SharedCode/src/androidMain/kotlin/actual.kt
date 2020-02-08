68318155715356a19023373d322713e56319155e
stNamco <lbnamco15@gmail.com>
2020-02-08 23:09:32 +0900

    added actual code to shared ios

--- SharedCode/src/androidMain/kotlin/actual.kt
+++ SharedCode/src/androidMain/kotlin/actual.kt
@@ -1,4 +1,4 @@
-package com.jetbrains.handson.mpp.mobile
+package com.stnamco.example
 
 actual fun platformName(): String {
     return "Android"
--- SharedCode/src/commonMain/kotlin/common.kt
+++ SharedCode/src/commonMain/kotlin/common.kt
@@ -1,4 +1,4 @@
-package com.jetbrains.handson.mpp.mobile
+package com.stnamco.example
 
 expect fun platformName(): String
 

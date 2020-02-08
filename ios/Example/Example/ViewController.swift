b849d4211e123ad521e87555906ca46ed97ffcf3
stNamco <lbnamco15@gmail.com>
2020-02-08 23:34:54 +0900

    created ios project

---
+++ ios/Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by stnamco on 2020/02/08.
+//  Copyright © 2020 namco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

3fe283854cb8fc1f9fedc18c5290ce4eb4b122df
stNamco <lbnamco15@gmail.com>
2020-02-08 23:21:34 +0900

    enable to call createApplicationScreenMessage()

--- app/src/main/java/com/stnamco/example/MainActivity.kt
+++ app/src/main/java/com/stnamco/example/MainActivity.kt
@@ -2,11 +2,14 @@ package com.stnamco.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
     }
 }
